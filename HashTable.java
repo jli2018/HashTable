2b6c2389b1cba6fad4636e36e63ac5989ffa7ea2
jli2018 <jli2018@pingry.org>
2015-09-30 01:43:41 -0400

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -1,7 +1,9 @@
-// Jessica Li
-// 09/28/15
-
-
+/**
+ * My Primitive HashTable class.
+ * 
+ * @author Jessica Li
+ * @version 0, 09/30/15
+ */
 public class HashTable
 {
 	private Object[] htable; 
@@ -9,7 +11,7 @@ public class HashTable
 	private final double loadFactor = 0.6;
 
 
-	/*
+	/**
 	 * Default constructor. Initializes to capacity 100.
 	 */
 	public HashTable()
@@ -17,23 +19,28 @@ public class HashTable
 		htable = new Object[100];
 	}
 
-	/*
-	 * Initializes size of array to capacity.
+	/**
+	 * Constructor. Initializes size of array to capacity.
+	 * 
+	 * @param capacity	determines the capacity of the HashTable array htable
 	 */
 	public HashTable( int capacity )
 	{
 		htable = new Object[capacity];
 	}
 
-	/*
-	 * Puts the object in the hashtable. Deals with collisions by placing the object using quadratic system, I forgot the name. 
+	/**
+	 * Puts the object in the hashtable. Deals with collisions by placing the object using quadratic probing, 
+	 * but not with prime numbers?
 	 * Always takes the absolute value of the hashcode. 
-	 * Rehashes table is load factor is exceeded. 
+	 * Rehashes table is load factor is exceeded. Load factor is compared with population of array / capacity of array. 
+	 * 
+	 * @param obj	the object to be hashed
 	 */
 	public void put( Object obj )
 	{
-		int code = Math.abs( obj.hashCode() );								// how to deal with negatives?
-		code = code % ( htable.length + 1 );
+		int code = Math.abs( obj.hashCode() );				// how to deal with negatives?
+		code = code % ( htable.length );
 		int quad = 1; 
 		while ( htable[code] != null )
 		{
@@ -51,8 +58,10 @@ public class HashTable
 		}
 	}
 
-	/*
+	/**
 	 * Returns String representation of the HashTable.
+	 * 
+	 * @return	a string representing the contents of the htable array
 	 */
 	public String toString()
 	{
@@ -65,9 +74,10 @@ public class HashTable
 		return toReturn;
 	}
 
-	/*
+	/**
 	 * Doubles the size of the HashTable and rehashes each item contained within. 
-	 * Created new array of double size, assigns to htable. Temporary array stores values of old array for rehashing into new array.
+	 * Created new array of double size, assigned to htable. 
+	 * Temporary array tempTable stores values of old htable array for rehashing into new htable array.
 	 */
 	private void rehash()
 	{
@@ -84,15 +94,18 @@ public class HashTable
 
 	}
 
-	/*
-	 * Main method for testing program. Creats a HashTable object and puts in the same string 10 times. Prints the table 10 times.
+	/**
+	 * Main method for testing program. Creats a HashTable object and uses loop to put in the same string 10 times 
+	 * and print the table 10 times.
+	 * 
+	 * @param args	main method parameter
 	 */
 	public static void main( String[] args )
 	{
 		HashTable ht = new HashTable();
 
-		String s = "Jessica";						//why does this give a negative hashCode value?
-		//System.out.println( s.hashCode() );
+		String s = "Jessica";					//why does this give a negative hashCode value?
+		//System.out.println( s.hashCode() );			//prints out hashcode value for String s
 
 		for ( int i = 0; i < 10; i++ )
 		{
